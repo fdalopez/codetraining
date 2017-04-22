dd57b20aeff76aa97d13fd79eec4e53148869af8
Fernanda <mfdalopezm@gmail.com>
2017-04-22 22:46:59 +0200

    devoir code

---
+++ code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/pythonw
+# -*-coding:Utf-8 -*
+# Test de la fonction input
+ano = input("Escriba su año de nacimiento : ") #dialogue utilisateur
+
+ano = int(ano) #transforme la variable en int
+
+#Ma proposition initiale
+
+if ano%4==0 and not ano%100==0:
+	print ("El ano seleccionado es un ano bisesto, usted es una persona especial")
+elif ano%400==0:
+	print ("El ano seleccionado es un ano bisiesto, usted es una persona especial")
+else:
+	print ("El ano seleccionado NO es un ano bisiesto, usted es una persona ordinaria")
